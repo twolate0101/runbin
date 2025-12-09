99c604a96cedf40103f930463b8b2adc51d01f70
xkm <xkm@xkm.be>
2025-12-09 14:52:08 +0800

    fix: #6
    - change div to pre to allow '\n'
    - change lsp css to allow '\n'
    - delete web/dist

--- web/dist/config.js
+++
@@ -1,9 +0,0 @@
-window.CONFIG = {
-    BACKEND: "https://fuhncvpxdcuf.ap-northeast-1.clawcloudrun.com",
-    LSP_SERVER: "wss://lsp.hxzzz.asia/cpp"
-}
-
-window.CONFIG = {
-    BACKEND: "https://example.com",
-    LSP_SERVER: "wss://examplelsp.com/cpp"
-}
